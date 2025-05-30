f84dbe4ccebe650778fabbfc0f7f3e5fbcc53cd1
dushyantsolanki <fullmoj70@gmail.com>
2025-05-30 19:47:52 +0530

    fixed a issue of a privacy.

--- backend/configs/firebase.config.js
+++ backend/configs/firebase.config.js
@@ -1,6 +1,16 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../taskmate-cb773-firebase-adminsdk-fbsvc-1c12d4fb08.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Build absolute path to JSON
+const serviceAccountPath = path.resolve(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+export default admin;
