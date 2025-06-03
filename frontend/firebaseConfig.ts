8a16e0593b4de27f9781aab6f97e2de49c0ebbfe
dushyantsolanki <fullmoj70@gmail.com>
2025-06-03 18:35:14 +0530

    secure a firebase json file

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -2,14 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_ID || 'AIzaSyB3gHx6FXAvqLx28MHZavCBUAjjwihULpM',
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN || 'taskmate-cb773.firebaseapp.com',
-  projectId: import.meta.env.VITE_PROJECT_ID || 'taskmate-cb773',
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || 'taskmate-cb773.firebasestorage.app',
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID || '682821712421',
-  appId: import.meta.env.VITE_APP_ID || '1:682821712421:web:4ffc1cbd26a3fa78f25dcc',
-  measurementId: import.meta.env.VITE_MEASUREMENTID || 'G-24RWZ64HN6',
+  apiKey: import.meta.env.VITE_APP_ID,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
+
+console.log('firebase config ::: ', firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 
