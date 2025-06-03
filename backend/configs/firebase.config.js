c67211bcdc9614d8547fe0b906299970c01ac47d
dushyantsolanki <fullmoj70@gmail.com>
2025-06-03 19:45:59 +0530

    update a firebase.config.js file

--- backend/configs/firebase.config.js
+++ backend/configs/firebase.config.js
@@ -1,17 +1,23 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../taskmate-cb773-firebase-adminsdk-fbsvc-768bbd6177.json' assert { type: 'json' };
-// import fs from 'fs';
-// import path from 'path';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// Build absolute path to JSON
-// const serviceAccountPath = path.resolve(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.GOOGLE_PROJECT_ID,
+      privateKey: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+      privateKeyId: process.env.GOOGLE_PRIVATE_KEY_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      authUri: process.env.GOOGLE_AUTH_URI,
+      tokenUri: process.env.GOOGLE_TOKEN_URI,
+      authProviderX509CertUrl: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+      clientC509CertUrl: process.env.GOOGLE_CLIENT_CERT_URL,
+      universeDomain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+    }),
+  });
+}
 
 export default admin;
