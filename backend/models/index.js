758c15db1f6cf7ac9545a85e7433d1a45d06732f
dushyantsolanki <fullmoj70@gmail.com>
2025-06-03 16:40:20 +0530

    Removed backend code from frontend branch

--- backend/models/index.js
+++
@@ -1,6 +0,0 @@
-import User from './user.model.js';
-import Role from './role.model.js';
-import Calendar from './calendar.model.js';
-import Notification from './notification.model.js';
-
-export { User, Role, Calendar, Notification };
