1f128659533b5bd8d9a3fa6fc2533655c01997ed
dushyantsolanki <fullmoj70@gmail.com>
2025-06-03 20:25:23 +0530

    api key name change.

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
